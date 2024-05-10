1a715c74807e299bf64db1ca9ac795c3512f7f27
rafeehcp <rafeeh@blend-ed.com>
2024-05-10 10:10:47 +0000

    chore: update npm dependencies for brand and frontend components

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -105,7 +105,7 @@ hooks.Filters.ENV_PATCHES.add_items(
         (
             "mfe-dockerfile-post-npm-install-learning",
             """
-RUN npm install '@edx/brand@git+https://github.com/rafeehcp/brand-openedx-xnetz.git'
+RUN npm install '@edx/brand@git+https://github.com/blend-ed/brand-openedx-xnetz.git'
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^1.0.0'
 RUN npm install '@edx/frontend-component-footer@git+https://github.com/blend-ed/frontend-component-footer-xnetz.git'
 """,
@@ -113,7 +113,7 @@ RUN npm install '@edx/frontend-component-footer@git+https://github.com/blend-ed/
         (
             "mfe-dockerfile-post-npm-install-authn",
             """
-RUN npm install '@edx/brand@git+https://github.com/rafeehcp/brand-openedx-xnetz.git'
+RUN npm install '@edx/brand@git+https://github.com/blend-ed/brand-openedx-xnetz.git'
 """,
         ),
         # Tutor-Indigo v2.1 targets the styling updations in discussions and learner-dashboard MFE
@@ -121,7 +121,7 @@ RUN npm install '@edx/brand@git+https://github.com/rafeehcp/brand-openedx-xnetz.
         (
             "mfe-dockerfile-post-npm-install-discussions",
             """
-RUN npm install '@edx/brand@git+https://github.com/rafeehcp/brand-openedx-xnetz.git'
+RUN npm install '@edx/brand@git+https://github.com/blend-ed/brand-openedx-xnetz.git'
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^1.0.0'
 RUN npm install '@edx/frontend-component-footer@git+https://github.com/blend-ed/frontend-component-footer-xnetz.git'
 """,
@@ -129,7 +129,7 @@ RUN npm install '@edx/frontend-component-footer@git+https://github.com/blend-ed/
         (
             "mfe-dockerfile-post-npm-install-learner-dashboard",
             """
-RUN npm install '@edx/brand@git+https://github.com/rafeehcp/brand-openedx-xnetz.git'
+RUN npm install '@edx/brand@git+https://github.com/blend-ed/brand-openedx-xnetz.git'
 RUN npm install '@edx/frontend-component-footer@git+https://github.com/blend-ed/frontend-component-footer-xnetz.git'
 """,
         ),
