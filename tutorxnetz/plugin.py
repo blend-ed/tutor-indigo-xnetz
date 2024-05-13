ec3fd589086906f7d6c57014d613f75c107c2777
nihxdr <nihadrahim99@gmail.com>
2024-05-13 23:45:57 +0530

    chore: Update package name and URLs for tutor-xnetz theme

--- tutorxnetz/plugin.py
+++ tutorxnetz/plugin.py
@@ -14,7 +14,7 @@ config = {
         "PRIMARY_COLOR": "#3b85ff",  # cool blue
         # Footer links are dictionaries with a "title" and "url"
         # To remove all links, run:
-        # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[] --set INDIGO_FOOTER_LEGAL_LINKS=[]
+        # tutor config save --set XNETZ_FOOTER_NAV_LINKS=[] --set XNETZ_FOOTER_LEGAL_LINKS=[]
         "FOOTER_NAV_LINKS": [
             {"title": "About", "url": "/about"},
             {"title": "Contact", "url": "/contact"},
@@ -22,8 +22,8 @@ config = {
         "FOOTER_LEGAL_LINKS": [
             {"title": "Terms of service", "url": "/tos"},
             {
-                "title": "Indigo theme for Open edX",
-                "url": "https://github.com/overhangio/tutor-indigo",
+                "title": "Xnetz Indigo theme for Open edX",
+                "url": "https://github.com/blend-ed/tutor-indigo-xnetz",
             },
         ],
     },
@@ -33,26 +33,26 @@ config = {
 
 # Theme templates
 hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
-    pkg_resources.resource_filename("tutorindigo", "templates")
+    pkg_resources.resource_filename("tutorxnetz", "templates")
 )
 # This is where the theme is rendered in the openedx build directory
 hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
     [
-        ("indigo", "build/openedx/themes"),
+        ("xnetz", "build/openedx/themes"),
     ],
 )
 
 # Force the rendering of scss files, even though they are included in a "partials" directory
 hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(
-    r"indigo/lms/static/sass/partials/lms/theme/"
+    r"xnetz/lms/static/sass/partials/lms/theme/"
 )
 
 # Load all configuration entries
 hooks.Filters.CONFIG_DEFAULTS.add_items(
-    [(f"INDIGO_{key}", value) for key, value in config["defaults"].items()]
+    [(f"XNETZ_{key}", value) for key, value in config["defaults"].items()]
 )
 hooks.Filters.CONFIG_UNIQUE.add_items(
-    [(f"INDIGO_{key}", value) for key, value in config["unique"].items()]
+    [(f"XNETZ_{key}", value) for key, value in config["unique"].items()]
 )
 hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
 
